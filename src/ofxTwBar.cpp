4c92c91777593e53cbad29368784c8330a5ab71d
Hugues Bruyere <hugues@Hugues-Bruyeres-MacBook-Pro-2.local>
2010-09-27 22:14:46 -0400

    initial commit

--- src/ofxTwBar.cpp
+++ src/ofxTwBar.cpp
@@ -4,7 +4,7 @@
  
  Bridge to AntTweakBar - "a light and intuitive graphical user interface" - <http://www.antisphere.com/Wiki/tools:anttweakbar>
 
- Code based on code from Cinder Lib.
+ Based on code from Cinder Lib.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  the following conditions are met:
